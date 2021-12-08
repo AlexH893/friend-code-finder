f86f0995cddd8174a98981ad43653f121867c941
Alex <31377763+AlexH893@users.noreply.github.com>
2021-12-08 16:04:06 -0700

    added timestamp

--- server/models/code.js
+++ server/models/code.js
@@ -6,10 +6,14 @@
 
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const moment = require("moment");
+const today = moment().format("MMM Do YYYY, h:mm:ss a");
 
 let codeSchema = new Schema(
   {
     code: { type: String },
+
+    date: { type: String, default: today },
   },
 
   { collection: "codes" }
